f3adab378dfe55bf402aefdb1555d2d0e29f6800
Raden Mohamad Adrian Ramadhan Hendar Wibawa <raden.mohamad11@ui.ac.id>
2024-06-04 09:54:41 +0700

    fix: fix business ogic on transaction

--- transaction/routes.py
+++ transaction/routes.py
@@ -3,7 +3,7 @@ import dotenv
 import json
 import os
 
-from fastapi import APIRouter, Request, Depends, HTTPException, status
+from fastapi import APIRouter, Request, Depends, HTTPException, status, Response
 from dependencies import authorize, generate_rerouter
 from dependencies.schemas import User
 from course.routes import get_course
@@ -163,7 +163,7 @@ async def get_transaction_list(id: str, user: User = Depends(authorize)):
 
 
 
-
+# DONE
 @transaction_router.post("/cart/add")
 async def create(request: Request, user: User = Depends(authorize)):
     body = await request.body()
@@ -178,6 +178,7 @@ async def create(request: Request, user: User = Depends(authorize)):
     
         request._headers = mutable_headers
         request.scope.update(headers=mutable_headers.raw)
+        
     except Exception as e:
         print(e)
         raise HTTPException(
@@ -187,7 +188,8 @@ async def create(request: Request, user: User = Depends(authorize)):
 
     return await rerouter(request=request)
 
-@transaction_router.post("/confirm/")
+# DONE
+@transaction_router.post("/confirm")
 async def create(request: Request, user: User = Depends(authorize)):
     body = await request.body()
     try:
@@ -196,102 +198,128 @@ async def create(request: Request, user: User = Depends(authorize)):
             "email": user.email,
             "first_name": user.username
         }
-
-        transaction_type = body_dict.get("type")
-        async with httpx.AsyncClient() as client:
-        
-            if transaction_type == "CART": # else would be SINGLE
-                resp = await client.get(
-                    url = SERVICE_URL + f"/transaction/course_owned/{user.email}"
-                )
-                if resp.status_code != 200:
-                    raise HTTPException(
-                        status_code=resp.status_code,
-                        detail=resp.json()['message']
-                    )
-                else:
-                    courses = resp.json()["course_list"]
-
-                    course_responses = []
-                    total_price = 0
-                    for course in courses:
-                        course_id = course["course_id"]
-
-                        resp = await client.get(
-                            url = COURSE_SERVICE_URL + f"/course/detail/{course_id}"
-                        )
-                        if resp.status_code != 200:
-                            raise HTTPException(
-                                status_code=resp.status_code,
-                                detail=resp.json()['detail']
-                            )
-                        else:
-                            course_responses.append(resp.json())
-                            total_price += resp.json()["price"]
-                    body_dict["item_details"] = [
-                        {
-                            "id": str(course["id"]),
-                            "quantity": 1,
-                            "price": course["price"],
-                            "name": course["title"],
-                        } for course in course_responses
-                    ]
-
-                    body_dict["transaction_details"] = {
-                        "gross_amount": total_price
-                    }
-            else:
-                course_id = body_dict.get("id")
-                resp = await client.get(
-                    url = COURSE_SERVICE_URL + f"course/detail/{course_id}"
-                )
-                if resp.status_code != 200:
-                    raise HTTPException(
-                        status_code=resp.status_code,
-                        detail=resp.json()['detail']
-                    )
-                else:
-                    course = resp.json()
-                    body_dict["item_details"] = [
-                        {
-                            "id": str(course["id"]),
-                            "quantity": 1,
-                            "price": course["price"],
-                            "name": course["title"],
-                        }
-                    ]
-
-                    body_dict["transaction_details"] = {
-                        "gross_amount": course["price"]
-                    }
-
-            body_dict.pop("type")
-            body_dict.pop("id") 
-
-
-
-
-        request._body = json.dumps(body_dict).encode()
-
-        mutable_headers = MutableHeaders(request._headers)
-        mutable_headers["content-length"] = str(len(request._body))
-    
-        request._headers = mutable_headers
-        request.scope.update(headers=mutable_headers.raw)
     except json.JSONDecodeError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Request body is not a valid JSON"
         )
 
-    return await rerouter(request=request)
 
+    transaction_type = body_dict.get("type")
+    async with httpx.AsyncClient() as client:
+    
+        if transaction_type == "CART": # else would be SINGLE
+            print("CART")
+            resp = await client.get(
+                url = SERVICE_URL + f"/transaction/cart/{user.email}"
+            )
+            if resp.status_code != 200:
+                raise HTTPException(
+                    status_code=resp.status_code,
+                    detail=resp.json()['message']
+                )
+            else:
+                courses = resp.json()["courses_in_cart"]
+
+                course_responses = []
+                total_price = 0
+                for course in courses:
+                    course_id = course["course_id"]
+
+                    resp = await client.get(
+                        url = COURSE_SERVICE_URL + f"/course/detail/{course_id}"
+                    )
+                    if resp.status_code != 200:
+                        raise HTTPException(
+                            status_code=resp.status_code,
+                            detail=resp.json()['detail']
+                        )
+                    else:
+                        course_responses.append(resp.json())
+                        total_price += resp.json()["price"]
+                body_dict["item_details"] = [
+                    {
+                        "id": str(course["id"]),
+                        "quantity": 1,
+                        "price": course["price"],
+                        "name": course["title"],
+                    } for course in course_responses
+                ]
+
+                body_dict["transaction_details"] = {
+                    "gross_amount": total_price
+                }
+                print(body_dict)
+        else:
+            print("SINGLE")
+            course_id = body_dict.get("id")
+            resp = await client.get(
+                url = COURSE_SERVICE_URL + f"/course/detail/{course_id}"
+            )
+            if resp.status_code != 200:
+                raise HTTPException(
+                    status_code=resp.status_code,
+                    detail=resp.json()['detail']
+                )
+            else:
+                course = resp.json()
+                body_dict["item_details"] = [
+                    {
+                        "id": str(course_id),
+                        "quantity": 1,
+                        "price": course["price"],
+                        "name": course["title"],
+                    }
+                ]
+
+                body_dict["transaction_details"] = {
+                    "gross_amount": course["price"]
+                }
+
+            
+
+            body_dict.pop("type")
+            body_dict.pop("id") 
+
+        request._body = json.dumps(body_dict).encode()
+
+        mutable_headers = MutableHeaders(request._headers)
+        mutable_headers["content-length"] = str(len(request._body))
+
+        request._headers = mutable_headers
+        request.scope.update(headers=mutable_headers.raw)
+
+
+        # return await rerouter(request=request)
+        # print(path)
+        url = SERVICE_URL + request.url.path
+        headers = request.headers.raw
+        method = request.method
+        body = json.dumps(body_dict).encode()
+
+        
+        resp = await client.request(
+            method = method,
+            url = url,
+            headers = headers,
+            content = body
+        )
+        print(resp.content)
+        print(resp.status_code)
+        return Response(
+            content=resp.content,
+            status_code=resp.status_code,
+            headers=resp.headers,
+        )
+
+# DONE
 @transaction_router.delete("/cart/remove")
 async def create(request: Request, user: User = Depends(authorize)):
     body = await request.body()
     try:
         body_dict: dict = json.loads(body)
         body_dict["email"] = user.email
+        body_dict["course_id"] = str(body_dict["course_id"])
         request._body = json.dumps(body_dict).encode()
 
         mutable_headers = MutableHeaders(request._headers)
@@ -307,6 +335,7 @@ async def create(request: Request, user: User = Depends(authorize)):
 
     return await rerouter(request=request)
 
+# DONE
 @transaction_router.delete("/cart/remove_all")
 async def create(request: Request, user: User = Depends(authorize)):
 
